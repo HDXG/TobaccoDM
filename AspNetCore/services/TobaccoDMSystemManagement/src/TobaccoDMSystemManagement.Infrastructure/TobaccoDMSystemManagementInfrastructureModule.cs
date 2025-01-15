b62dade5fe2e63c2d80119a08279b2e16598e1c5
cohen <1768065921@qq.com>
2025-01-15 22:48:03 +0800

    add TobaccoDMSystemManagement
    add TobaccoDMSystemManagement

---
+++ AspNetCore/services/TobaccoDMSystemManagement/src/TobaccoDMSystemManagement.Infrastructure/TobaccoDMSystemManagementInfrastructureModule.cs
@@ -0,0 +1,11 @@
+﻿using Dedsi.CleanArchitecture.Infrastructure;
+using Volo.Abp.Modularity;
+namespace TobaccoDMSystemManagement.Infrastructure;
+
+[DependsOn(
+    typeof(DedsiCleanArchitectureInfrastructureModule)
+    )]
+public class TobaccoDMSystemManagementInfrastructureModule:AbpModule
+{
+
+}
---
+++ AspNetCore/services/TobaccoDMSystemManagement/src/TobaccoDMSystemManagement.UseCase/TobaccoDMSystemManagementUseCaseModule.cs
@@ -0,0 +1,24 @@
+﻿using System.Reflection;
+using Dedsi.Ddd.CQRS;
+using Microsoft.Extensions.DependencyInjection;
+using TobaccoDMSystemManagement.Domain;
+using TobaccoDMSystemManagement.Infrastructure;
+using Volo.Abp.Modularity;
+namespace TobaccoDMSystemManagement.UseCase
+{
+    [DependsOn(
+        typeof(TobaccoDMSystemManagementDomainModule),
+        typeof(TobaccoDMSystemManagementInfrastructureModule),
+        typeof(DedsiDddCQRSModule)   
+        )]
+    public class TobaccoDMSystemManagementUseCaseModule:AbpModule
+    {
+        public override void ConfigureServices(ServiceConfigurationContext context)
+        {
+            context.Services.AddMediatR(opt =>
+            {
+                opt.RegisterServicesFromAssemblies(Assembly.GetExecutingAssembly());
+            });
+        }
+    }
+}
