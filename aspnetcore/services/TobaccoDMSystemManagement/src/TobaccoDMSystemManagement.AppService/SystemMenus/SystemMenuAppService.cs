e743de3c2762e8819b870cca90a3a8df74959720
HDNetXG <59039341+HDXG@users.noreply.github.com>
2025-02-07 22:06:55 +0800

    feat:Add菜单

--- aspnetcore/services/TobaccoDMSystemManagement/src/TobaccoDMSystemManagement.AppService/SystemMenus/SystemMenuAppService.cs
+++ aspnetcore/services/TobaccoDMSystemManagement/src/TobaccoDMSystemManagement.AppService/SystemMenus/SystemMenuAppService.cs
@@ -1,18 +1,35 @@
-﻿using TobaccoDMSystemManagement.Domain.SystemMenus;
+﻿using TobaccoDMSystemManagement.AppService.SystemMenus.Dtos;
+using TobaccoDMSystemManagement.Domain.SystemMenus;
 using TobaccoDMSystemManagement.Infrastructure.Repositories.SystemMenus;
+using TobaccoDMSystemManagement.Infrastructure.Utils;
 
 namespace TobaccoDMSystemManagement.AppService.SystemMenus
 {
     public interface ISystemMenuAppService : ITobaccoDMSystemManagementAppService
     {
-        Task<bool> ChangeSystemMenu();
+        Task<bool> ChangeSystemMenu(SystemMenuDto request);
+
+        Task<SystemMenuDto> GetSystemMenu(Guid id);
     }
 
-    public class SystemMenuAppService(ISystemMenuRepository _systemMenusRepository) : TobaccoDMSystemManagementAppService, ISystemMenuAppService
+    public class SystemMenuAppService(ISystemMenuRepository systemMenuRepository) : TobaccoDMSystemManagementAppService, ISystemMenuAppService
     {
-        public Task<bool> ChangeSystemMenu()
+        public Task<bool> ChangeSystemMenu(SystemMenuDto request)
         {
-           return  _systemMenusRepository.InsertAsync(new SystemMenu());
+            SystemMenu systemMenu = MapsterConfig.MapsterTo<SystemMenuDto, SystemMenu>(request);
+            if (request.Id == Guid.Empty)
+            {
+                systemMenu.CreateIdTime(GuidGenerator.Create());
+                systemMenu.SavaUpdateTime();
+                return systemMenuRepository.InsertAsync(systemMenu);
+            }
+            else
+            {
+                systemMenu.SavaUpdateTime();
+                return systemMenuRepository.UpdateAsync(systemMenu);
+            }
         }
+
+        public async Task<SystemMenuDto> GetSystemMenu(Guid id) => MapsterConfig.MapsterTo<SystemMenu, SystemMenuDto>(await systemMenuRepository.GetAsync(x => x.Id == id));
     }
 }
--- aspnetcore/services/TobaccoDMSystemManagement/src/TobaccoDMSystemManagement.HttpApi/SystemMenus/SystemMenuController.cs
+++ aspnetcore/services/TobaccoDMSystemManagement/src/TobaccoDMSystemManagement.HttpApi/SystemMenus/SystemMenuController.cs
@@ -1,13 +1,24 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using TobaccoDMSystemManagement.AppService.SystemMenus;
+using TobaccoDMSystemManagement.AppService.SystemMenus.Dtos;
 namespace TobaccoDMSystemManagement.SystemMenus
 {
     /// <summary>
     /// 系统添加菜单
     /// </summary>
-    public class SystemMenuController(ISystemMenuAppService _SystemMenu):TobaccoDMSystemManagementController
+    public class SystemMenuController(ISystemMenuAppService systemMenuAppService):TobaccoDMSystemManagementController
     {
+        /// <summary>
+        /// 保存菜单
+        /// </summary>
+        /// <param name="menuDto"></param>
+        /// <returns></returns>
+        [HttpPost]
+        public Task<bool> ChangeSystemMenu(SystemMenuDto menuDto) => systemMenuAppService.ChangeSystemMenu(menuDto);
 
-       
+        
+        [HttpGet]
+        public Task<SystemMenuDto> GetSystemMenu(Guid Id) => systemMenuAppService.GetSystemMenu(Id);
+
     }
 }
